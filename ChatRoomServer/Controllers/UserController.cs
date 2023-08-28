d62d18d8490ddce562ee3d2ec480c1b01ae0f3d0
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-08-28 14:42:10 -0400

    username/password header encryption testing

--- ChatRoomServer/Controllers/UserController.cs
+++ ChatRoomServer/Controllers/UserController.cs
@@ -2,9 +2,11 @@
 using ChatRoomServer.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace ChatRoomServer.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         private readonly ILogger<ChatController> _logger;
@@ -43,9 +45,36 @@ namespace ChatRoomServer.Controllers
             return StatusCode(406, "Username and/or password is incorrect");
         }
 
+        // DEBUG
         [AllowAnonymous]
-        public IActionResult Register(string username, string password)
+        public IActionResult Encode(string username, string password)
         {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes($"{username}:{password}");
+            return Ok(Convert.ToBase64String(plainTextBytes));
+        }
+
+        // DEBUG
+        [AllowAnonymous]
+        public IActionResult Register2([FromHeader(Name = "user")] string user)
+        {
+            try
+            {
+                byte[] base64Encoded = Convert.FromBase64String(user);
+                string decoded = Encoding.UTF8.GetString(base64Encoded);
+                string[] users = decoded.Split(':');
+                return Ok($"Username: {users[0]}, Password: {users[1]}");
+            }
+            catch
+            {
+                return StatusCode(500, "Error processing input");
+            }
+        }
+
+        [AllowAnonymous]
+        public IActionResult Register([FromHeader(Name = "username")] string username, [FromHeader(Name = "password")] string password)
+        {
+            if (username == null || password == null)
+                return StatusCode(406, "Invalid input");
             int hash = password.GetHashCode();
             User user = new User() { Username = username, PasswordHash = hash, Id = (_context.Users.LastOrDefault()?.Id ?? -1) + 1 };
             if (_context.Users.Where(n => n.Username == username).Count() > 0)
