cb94648819c10ca863d4f4d49a170738e03a3b57
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-09-04 02:43:53 -0400

    beginning of chat room console demo client

---
+++ ChatRoomDemoClient/AuthenticationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoomDemoClient
+{
+    internal static class AuthenticationHandler
+    {
+        public static void GetCredentials(out string? username, out string? password)
+        {
+            Console.Write("Enter username: ");
+            username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
+        }
+
+        public static void Authenticate(string username, string password)
+        {
+
+        }
+    }
+}
---
+++ ChatRoomDemoClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using ChatRoomDemoClient;
+using System.Net;
+using System.Net.Http.Headers;
+
+HttpClient _client = new()
+{
+    BaseAddress = new Uri("https://localhost:7185")
+};
+
+char loginChoice;
+Console.WriteLine("Select an option\n1. Login\n2. Register an account");
+do
+{
+    loginChoice = (char)Console.Read();
+}
+while (loginChoice != '1' && loginChoice != '2');
+
+AuthenticationHandler.GetCredentials(out string? username, out string? password);
+while (username == null || password == null)
+{
+    Console.WriteLine("Invalid entry, please try again");
+    AuthenticationHandler.GetCredentials(out username, out password);
+
+}
+
+if (loginChoice == 2)
+{
+    using HttpResponseMessage response = await _client.GetAsync("user");
+
+    response.EnsureSuccessStatusCode().WriteRequestToConsole();
+
+    var responseData = await response.Content.ReadAsStringAsync();
+    Console.WriteLine($"{responseData}\n");
+}
