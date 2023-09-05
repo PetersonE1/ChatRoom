7ead78877221294a600bc7acf0e20f24e2d97d31
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-09-05 15:56:55 -0400

    setting up basic behaviour

--- ChatRoomClient1/MainWindow.xaml.cs
+++ ChatRoomClient1/MainWindow.xaml.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,7 @@ namespace ChatRoomClient
     public partial class MainWindow : Window
     {
         HttpClient _client;
+        WebSocket? _webSocket;
 
         public MainWindow()
         {
@@ -30,8 +33,8 @@ namespace ChatRoomClient
             {
                 BaseAddress = new Uri("https://localhost:7185")
             };
-            ChatScreen.IsEnabled = false;
             InitializeComponent();
+            ChatScreen.Visibility = Visibility.Collapsed;
         }
 
         private async void B_Login_Click(object sender, RoutedEventArgs e)
@@ -40,7 +43,9 @@ namespace ChatRoomClient
             string userHash = userpass.ToBase64();
             await AuthenticationHandler.Authenticate(userHash, _client);
             LoginScreen.IsEnabled = false;
+            LoginScreen.Visibility = Visibility.Collapsed;
             ChatScreen.IsEnabled = true;
+            ChatScreen.Visibility = Visibility.Visible;
         }
 
         private async void B_Register_Click(object sender, RoutedEventArgs e)
@@ -50,7 +55,42 @@ namespace ChatRoomClient
             await AuthenticationHandler.Register(userHash, _client);
             await AuthenticationHandler.Authenticate(userHash, _client);
             LoginScreen.IsEnabled = false;
+            LoginScreen.Visibility = Visibility.Collapsed;
             ChatScreen.IsEnabled = true;
+            ChatScreen.Visibility = Visibility.Visible;
+        }
+
+        private async void B_RefreshLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string userpass = I_Username.Text + ":" + I_Password.Password;
+            string userHash = userpass.ToBase64();
+            await AuthenticationHandler.Authenticate(userHash, _client);
+        }
+
+        private async void B_Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (_webSocket != null)
+            {
+                await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client closed", default);
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_webSocket == null)
+                return;
+
+            string s = I_Command.Text.ToBase64();
+
+            await _webSocket.SendAsync(
+                            new ArraySegment<byte>(Encoding.UTF8.GetBytes(s), 0, s.Length),
+                            WebSocketMessageType.Binary,
+                            true,
+                            CancellationToken.None);
+
+            var bytes = new byte[1024];
+            var result = await _webSocket.ReceiveAsync(bytes, default);
+            string res = Encoding.UTF8.GetString(bytes, 0, result.Count);
         }
     }
 }
