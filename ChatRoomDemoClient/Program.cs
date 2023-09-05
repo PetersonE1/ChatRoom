8d106e321c0c5d906fecba76ed0d16184aa18104
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-09-05 09:51:36 -0400

    client loops, set up to send message codes

--- ChatRoomDemoClient/Program.cs
+++ ChatRoomDemoClient/Program.cs
@@ -40,7 +40,7 @@ using ClientWebSocket ws = new();
 ws.Options.SetRequestHeader("Authorization", $"Bearer {AuthenticationHandler._token}");
 await ws.ConnectAsync(new Uri("wss://localhost:7185/chat/EstablishConnection"), new HttpMessageInvoker(handler), default);
 
-string[] toSend = { "first", "second", "third" };
+/*string[] toSend = { "first", "second", "third" };
 string s = string.Empty;
 foreach (string st in toSend)
     s += Convert.ToBase64String(Encoding.UTF8.GetBytes(st)) + ':';
@@ -58,4 +58,41 @@ string res = Encoding.UTF8.GetString(bytes, 0, result.Count);
 
 await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client closed", default);
 
-Console.WriteLine(res);
+Console.WriteLine(res);*/
+
+Console.WriteLine("Choose an option:\n1. Send message\n2. Send server request\n3. Close connection");
+string s = string.Empty;
+while (s != "CLOSE")
+{
+    s = string.Empty;
+    do
+    {
+        switch (Console.ReadKey(true).KeyChar)
+        {
+            case '1': s = "m-"; break;
+            case '2': s = "r-"; break;
+            case '3': s = "CLOSE"; break;
+            default: break;
+        }
+    }
+    while (s == string.Empty);
+
+    if (s == "CLOSE")
+        break;
+
+    string message = Console.ReadLine() ?? string.Empty;
+    s += Convert.ToBase64String(Encoding.UTF8.GetBytes(message));
+
+    await ws.SendAsync(
+                    new ArraySegment<byte>(Encoding.UTF8.GetBytes(s), 0, s.Length),
+                    WebSocketMessageType.Text,
+                    true,
+                    CancellationToken.None);
+
+    var bytes = new byte[1024];
+    var result = await ws.ReceiveAsync(bytes, default);
+    string res = Encoding.UTF8.GetString(bytes, 0, result.Count);
+    Console.WriteLine(res);
+}
+
+await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client closed", default);
